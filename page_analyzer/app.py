829246fdda68ab7e1b8a8ff5ea272c08886556de
Iris-Raine <iris.raine.darkest.star@gmail.com>
2023-01-06 13:29:49 +0000

    minor validator rollback

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -104,8 +104,6 @@ def check_url(id):
     with connection.cursor(cursor_factory=NamedTupleCursor) as cursor:
         cursor.execute("SELECT * FROM urls WHERE id=%s;", (id, ))
         url = cursor.fetchone()
-        if not validators.url(url.name):
-            flash('Произошла ошибка при проверке', 'danger')
         response = http_request(url.name)
         if not response:
             flash('Произошла ошибка при проверке', 'danger')
